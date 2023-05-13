0d086b58c0edd0f03db7ba3f6d0cea34d6c9bba4
SkyMolfar <skymolfars@gmail.com>
2023-05-13 13:26:13 +0200

    small changes in the code

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,20 +3,25 @@ import SimpleLightbox from 'simplelightbox';
 // Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items.js';
-// Change code below this lin
+
+// Change code below this line
 function generateGalleryItem(item) {
   let template = `<li class="gallery__item">
-  <a class="gallery__link" href="${item.original}">
-     <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
-  </a>
-</li>`;
+    <a class="gallery__link" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>
+  </li>`;
   return template;
 }
+
 const gallery = document.querySelector('.gallery');
 console.log(gallery);
-gallery.innerHTML = galleryItems.map(generateGalleryItem).join('');
+
+const galleryMarkup = galleryItems.map(generateGalleryItem).join('');
+gallery.innerHTML = galleryMarkup;
 
 new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
 });
+
